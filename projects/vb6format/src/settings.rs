7d784a681eab60b34ef5a522aac9fe7b3cf0ceac
ScriptAndCompile <scriptandcompile@gmail.com>
2026-07-29 13:20:03 -0700

    added loading settings from .aspenfmt.toml settings file and added keyword flag

--- projects/vb6format/src/settings.rs
+++ projects/vb6format/src/settings.rs
@@ -1,5 +1,6 @@
 pub struct FmtSettings {
     pub indent_size: usize,
+    pub keyword_case: String,
     pub blank_lines_around_directives: bool,
     pub blank_lines_inside_directives: bool,
 }
@@ -8,6 +9,7 @@ impl Default for FmtSettings {
     fn default() -> Self {
         Self {
             indent_size: 4,
+            keyword_case: "camel".to_string(),
             blank_lines_around_directives: false,
             blank_lines_inside_directives: false,
         }
