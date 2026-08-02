910bc4b9fb771ce6919b39db024826e5da9a4458
ScriptAndCompile <scriptandcompile@gmail.com>
2026-08-01 22:55:43 -0700

    wasm wasn't being built and accepted. just force it.

--- docs/vb6parse/assets/wasm/vb6parse.d.ts
+++ docs/vb6parse/assets/wasm/vb6parse.d.ts
@@ -2,28 +2,6 @@
 /* eslint-disable */
 
 /**
- * Chroma subsampling format
- */
-export enum ChromaSampling {
-    /**
-     * Both vertically and horizontally subsampled.
-     */
-    Cs420 = 0,
-    /**
-     * Horizontally subsampled.
-     */
-    Cs422 = 1,
-    /**
-     * Not subsampled.
-     */
-    Cs444 = 2,
-    /**
-     * Monochrome.
-     */
-    Cs400 = 3,
-}
-
-/**
  * Initializes the panic hook for better error messages in the browser console.
  */
 export function init_panic_hook(): void;
@@ -64,15 +42,13 @@ export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembl
 
 export interface InitOutput {
     readonly memory: WebAssembly.Memory;
+    readonly parse_vb6_code: (a: number, b: number, c: number, d: number, e: number) => void;
+    readonly tokenize_vb6_code: (a: number, b: number, c: number) => void;
     readonly init_panic_hook: () => void;
-    readonly parse_vb6_code: (a: number, b: number, c: number, d: number) => [number, number, number];
-    readonly tokenize_vb6_code: (a: number, b: number) => [number, number, number];
-    readonly __wbindgen_malloc: (a: number, b: number) => number;
-    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
-    readonly __wbindgen_free: (a: number, b: number, c: number) => void;
-    readonly __wbindgen_externrefs: WebAssembly.Table;
-    readonly __externref_table_dealloc: (a: number) => void;
-    readonly __wbindgen_start: () => void;
+    readonly __wbindgen_export: (a: number, b: number) => number;
+    readonly __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+    readonly __wbindgen_export3: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
 }
 
 export type SyncInitInput = BufferSource | WebAssembly.Module;
