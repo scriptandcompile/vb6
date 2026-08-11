99cee7a443ec0462bf658e3b820e4da79235f108
ScriptAndCompile <scriptandcompile@gmail.com>
2026-08-10 23:47:32 -0700

    update interpret playground

---
+++ docs/vb6interpret/assets/wasm/vb6interpret_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const build_debug_trace: (a: number, b: number, c: number) => void;
+export const debug_vb6_code: (a: number, b: number, c: number, d: number) => void;
+export const interpret_vb6_code: (a: number, b: number, c: number) => void;
+export const parse_vb6_code: (a: number, b: number, c: number, d: number, e: number) => void;
+export const tokenize_vb6_code: (a: number, b: number, c: number) => void;
+export const init_panic_hook: () => void;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export3: (a: number, b: number, c: number) => void;
+export const __wbindgen_add_to_stack_pointer: (a: number) => number;
