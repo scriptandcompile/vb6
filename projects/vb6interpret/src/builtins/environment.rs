d349e148c5a06d65aa772258561004cca558c25f
ScriptAndCompile <scriptandcompile@gmail.com>
2026-08-16 21:58:24 -0700

    imestatus (defaulting to zero / disabled at the moment).

--- projects/vb6interpret/src/builtins/environment.rs
+++ projects/vb6interpret/src/builtins/environment.rs
@@ -11,8 +11,8 @@ use vb6runtime::library::environment::environ::environ;
 use vb6runtime::library::environment::environ_dollar::environ_dollar;
 use vb6runtime::library::environment::error::error;
 use vb6runtime::library::environment::error_dollar::error_dollar;
-use vb6runtime::library::environment::getsetting::get_setting;
 use vb6runtime::library::environment::getallsettings::get_all_settings;
+use vb6runtime::library::environment::getsetting::get_setting;
 use vb6runtime::library::environment::savesetting::save_setting;
 use vb6runtime::VBVariant;
 
@@ -38,6 +38,9 @@ pub(super) fn register(registry: &mut Registry) {
     }));
     registry.insert(builtin!("savesetting", 4, 4, |args| {
         save_setting(&args[0], &args[1], &args[2], &args[3])
+    }));
+    registry.insert(builtin!("imestatus", 0, 0, |_args| {
+        vb6runtime::library::environment::imestatus::imestatus()
     }));
     registry.insert(builtin!("getallsettings", 2, 2, |args| {
         get_all_settings(&args[0], &args[1])
