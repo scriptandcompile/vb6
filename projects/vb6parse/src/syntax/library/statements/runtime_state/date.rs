74d01fb06e99dc6e1d99f6632d83fd339ef12acd
ScriptAndCompile <scriptandcompile@gmail.com>
2026-08-25 15:50:07 -0700

    added parse_date_statement which now correctly parses the date statement with an expression tree instead of a flat consumption.

--- projects/vb6parse/src/syntax/library/statements/runtime_state/date.rs
+++ projects/vb6parse/src/syntax/library/statements/runtime_state/date.rs
@@ -16,7 +16,35 @@ use crate::parsers::cst::Parser;
 
 impl Parser<'_> {
     pub(crate) fn parse_date_statement(&mut self) {
-        self.parse_simple_builtin_statement(SyntaxKind::DateStatement);
+        self.parsing_header = false;
+
+        self.builder.start_node(SyntaxKind::DateStatement.to_raw());
+
+        self.consume_whitespace();
+
+        if let Some((text, token)) = self.tokens.get(self.pos) {
+            let kind = SyntaxKind::from(*token);
+            if kind == SyntaxKind::DateKeyword {
+                self.builder.token(SyntaxKind::DateKeyword.to_raw(), text);
+                self.pos += 1;
+            }
+        }
+
+        self.consume_whitespace();
+
+        if let Some((text, token)) = self.tokens.get(self.pos) {
+            let kind = SyntaxKind::from(*token);
+            if kind == SyntaxKind::EqualityOperator {
+                self.builder.token(kind.to_raw(), text);
+                self.pos += 1;
+            }
+        }
+
+        self.consume_whitespace();
+
+        self.parse_expression();
+
+        self.builder.finish_node();
     }
 }
 
