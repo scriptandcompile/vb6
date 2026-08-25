b21be9d41818ec1a08ae1da04fb61bada86a6690
ScriptAndCompile <scriptandcompile@gmail.com>
2026-08-25 15:29:57 -0700

    added parse_mid_statement which now correctly parses mid with an expression tree instead of flat parsing.

--- projects/vb6parse/src/syntax/library/statements/string_manipulation/mid.rs
+++ projects/vb6parse/src/syntax/library/statements/string_manipulation/mid.rs
@@ -40,13 +40,75 @@
 //!
 //! [Mid Statement - Microsoft Docs](https://learn.microsoft.com/en-us/office/vba/language/reference/user-interface-help/mid-statement)
 
+use crate::language::Token;
 use crate::parsers::cst::Parser;
 use crate::parsers::syntaxkind::SyntaxKind;
 
 impl Parser<'_> {
-    /// Parses a Mid statement.
+    /// Parses a Mid statement: `Mid(stringvar, start[, length]) = string`
     pub(crate) fn parse_mid_statement(&mut self) {
-        self.parse_simple_builtin_statement(SyntaxKind::MidStatement);
+        self.parsing_header = false;
+
+        self.builder.start_node(SyntaxKind::MidStatement.to_raw());
+
+        self.consume_whitespace();
+
+        if let Some((text, _)) = self.tokens.get(self.pos) {
+            self.builder
+                .token(SyntaxKind::MidKeyword.to_raw(), text);
+            self.pos += 1;
+        }
+
+        self.consume_whitespace();
+
+        if self.at_token(Token::LeftParenthesis) {
+            self.consume_token();
+        }
+
+        self.builder.start_node(SyntaxKind::ArgumentList.to_raw());
+
+        if self.is_at_end() || self.at_token(Token::RightParenthesis) {
+            // No arguments, just closing paren
+        } else {
+            // Parse first argument
+            self.builder.start_node(SyntaxKind::Argument.to_raw());
+            self.parse_expression();
+            self.builder.finish_node();
+
+            // Parse additional arguments separated by commas
+            while !self.is_at_end() && !self.at_token(Token::RightParenthesis) {
+                self.consume_whitespace();
+                if !self.is_at_end() && self.at_token(Token::Comma) {
+                    self.consume_token();
+                }
+                self.consume_whitespace();
+                if !self.is_at_end() && !self.at_token(Token::RightParenthesis) {
+                    self.builder.start_node(SyntaxKind::Argument.to_raw());
+                    self.parse_expression();
+                    self.builder.finish_node();
+                }
+            }
+        }
+
+        self.builder.finish_node();
+
+        if self.at_token(Token::RightParenthesis) {
+            self.consume_token();
+        }
+
+        self.consume_whitespace();
+
+        if let Some((text, token)) = self.tokens.get(self.pos) {
+            let kind = SyntaxKind::from(*token);
+            if kind == SyntaxKind::EqualityOperator {
+                self.builder.token(kind.to_raw(), text);
+                self.pos += 1;
+            }
+        }
+
+        self.parse_expression();
+
+        self.builder.finish_node();
     }
 }
 
