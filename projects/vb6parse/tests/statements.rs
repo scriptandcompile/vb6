2ab1a606d8a614f01778be7457c59ba96a0e233b
ScriptAndCompile <scriptandcompile@gmail.com>
2026-08-26 05:05:31 -0700

    moved tests for lock into file_operations

--- projects/vb6parse/tests/statements.rs
+++ projects/vb6parse/tests/statements.rs
@@ -35,3 +35,6 @@ mod input;
 
 #[path = "statements/file_operations/line_input.rs"]
 mod line_input;
+
+#[path = "statements/file_operations/lock.rs"]
+mod lock;
---
+++ projects/vb6parse/tests/statements/file_operations/lock.rs
@@ -0,0 +1,266 @@
+use vb6parse::ConcreteSyntaxTree;
+
+#[test]
+fn lock_simple() {
+    let source = r"
+Sub Test()
+    Lock #1
+End Sub
+";
+    let (cst_opt, failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
+    assert_eq!(failures.len(), 0, "Expected no parse failures.");
+    let cst = cst_opt.expect("CST should be parsed");
+
+    let tree = cst.to_serializable();
+
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../../../snapshots/syntax/statements/file_operations/lock");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
+}
+
+#[test]
+fn lock_at_module_level() {
+    let source = "Lock #1\n";
+    let (cst_opt, failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
+    assert_eq!(failures.len(), 0, "Expected no parse failures.");
+    let cst = cst_opt.expect("CST should be parsed");
+
+    let tree = cst.to_serializable();
+
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../../../snapshots/syntax/statements/file_operations/lock");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
+}
+
+#[test]
+fn lock_entire_file() {
+    let source = r"
+Sub Test()
+    Lock #1
+End Sub
+";
+    let (cst_opt, failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
+    assert_eq!(failures.len(), 0, "Expected no parse failures.");
+    let cst = cst_opt.expect("CST should be parsed");
+
+    let tree = cst.to_serializable();
+
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../../../snapshots/syntax/statements/file_operations/lock");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
+}
+
+#[test]
+fn lock_single_record() {
+    let source = r"
+Sub Test()
+    Lock #1, 5
+End Sub
+";
+    let (cst_opt, failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
+    assert_eq!(failures.len(), 0, "Expected no parse failures.");
+    let cst = cst_opt.expect("CST should be parsed");
+
+    let tree = cst.to_serializable();
+
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../../../snapshots/syntax/statements/file_operations/lock");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
+}
+
+#[test]
+fn lock_record_range() {
+    let source = r"
+Sub Test()
+    Lock #1, 10 To 20
+End Sub
+";
+    let (cst_opt, failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
+    assert_eq!(failures.len(), 0, "Expected no parse failures.");
+    let cst = cst_opt.expect("CST should be parsed");
+
+    let tree = cst.to_serializable();
+
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../../../snapshots/syntax/statements/file_operations/lock");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
+}
+
+#[test]
+fn lock_with_variable() {
+    let source = r"
+Sub Test()
+    Lock fileNum, recordNum
+End Sub
+";
+    let (cst_opt, failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
+    assert_eq!(failures.len(), 0, "Expected no parse failures.");
+    let cst = cst_opt.expect("CST should be parsed");
+
+    let tree = cst.to_serializable();
+
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../../../snapshots/syntax/statements/file_operations/lock");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
+}
+
+#[test]
+fn lock_preserves_whitespace() {
+    let source = "    Lock    #1  ,  5    \n";
+    let (cst_opt, failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
+    assert_eq!(failures.len(), 0, "Expected no parse failures.");
+    let cst = cst_opt.expect("CST should be parsed");
+
+    let tree = cst.to_serializable();
+
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../../../snapshots/syntax/statements/file_operations/lock");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
+}
+
+#[test]
+fn lock_with_comment() {
+    let source = r"
+Sub Test()
+    Lock #1, 5 ' Lock record 5
+End Sub
+";
+    let (cst_opt, failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
+    assert_eq!(failures.len(), 0, "Expected no parse failures.");
+    let cst = cst_opt.expect("CST should be parsed");
+
+    let tree = cst.to_serializable();
+
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../../../snapshots/syntax/statements/file_operations/lock");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
+}
+
+#[test]
+fn lock_in_if_statement() {
+    let source = r"
+Sub Test()
+    If needsLock Then
+        Lock #1, currentRecord
+    End If
+End Sub
+";
+    let (cst_opt, failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
+    assert_eq!(failures.len(), 0, "Expected no parse failures.");
+    let cst = cst_opt.expect("CST should be parsed");
+
+    let tree = cst.to_serializable();
+
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../../../snapshots/syntax/statements/file_operations/lock");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
+}
+
+#[test]
+fn lock_inline_if() {
+    let source = r"
+Sub Test()
+    If multiUser Then Lock #1
+End Sub
+";
+    let (cst_opt, failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
+    assert_eq!(failures.len(), 0, "Expected no parse failures.");
+    let cst = cst_opt.expect("CST should be parsed");
+
+    let tree = cst.to_serializable();
+
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../../../snapshots/syntax/statements/file_operations/lock");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
+}
+
+#[test]
+fn lock_unlock_pair() {
+    let source = r"
+Sub Test()
+    Lock #1, 5
+    ' Do work
+    Unlock #1, 5
+End Sub
+";
+    let (cst_opt, failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
+    assert_eq!(failures.len(), 0, "Expected no parse failures.");
+    let cst = cst_opt.expect("CST should be parsed");
+
+    let tree = cst.to_serializable();
+
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../../../snapshots/syntax/statements/file_operations/lock");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
+}
+
+#[test]
+fn lock_with_error_handling() {
+    let source = r#"
+Sub Test()
+    On Error Resume Next
+    Lock #1, recordNum
+    If Err.Number <> 0 Then
+        MsgBox "Could not lock record"
+    End If
+End Sub
+"#;
+    let (cst_opt, failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
+    assert_eq!(failures.len(), 0, "Expected no parse failures.");
+    let cst = cst_opt.expect("CST should be parsed");
+
+    let tree = cst.to_serializable();
+
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../../../snapshots/syntax/statements/file_operations/lock");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
+}
+
+#[test]
+fn lock_for_shared_file_access() {
+    let source = r#"
+Sub Test()
+    Open "data.dat" For Random As #1 Shared
+    Lock #1, 10
+    ' Write to record 10
+    Put #1, 10, myData
+    Unlock #1, 10
+    Close #1
+End Sub
+"#;
+    let (cst_opt, failures) = ConcreteSyntaxTree::from_text("test.bas", source).unpack();
+    assert_eq!(failures.len(), 0, "Expected no parse failures.");
+    let cst = cst_opt.expect("CST should be parsed");
+
+    let tree = cst.to_serializable();
+
+    let mut settings = insta::Settings::clone_current();
+    settings.set_snapshot_path("../../../snapshots/syntax/statements/file_operations/lock");
+    settings.set_prepend_module_to_snapshot(false);
+    let _guard = settings.bind_to_scope();
+    insta::assert_yaml_snapshot!(tree);
+}
