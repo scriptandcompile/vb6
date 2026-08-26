afc3f37b888d7098b91218b0924c4ed7b01a6907
ScriptAndCompile <scriptandcompile@gmail.com>
2026-08-25 18:31:09 -0700

    added parse_app_activate_statement which now correctly parses the app_activate statement with an expression tree instead of a flat consumption.

--- projects/vb6parse/src/syntax/library/statements/system_interaction/app_activate.rs
+++ projects/vb6parse/src/syntax/library/statements/system_interaction/app_activate.rs
@@ -1,6 +1,7 @@
 use crate::parsers::SyntaxKind;
 
 use crate::parsers::cst::Parser;
+use crate::Token;
 
 impl Parser<'_> {
     // VB6 AppActivate statement syntax:
@@ -10,7 +11,17 @@ impl Parser<'_> {
     //
     // [Reference](https://learn.microsoft.com/en-us/office/vba/language/reference/user-interface-help/appactivate-statement)
     pub(crate) fn parse_app_activate_statement(&mut self) {
-        self.parse_simple_builtin_statement(SyntaxKind::AppActivateStatement);
+        self.builder.start_node(SyntaxKind::AppActivateStatement.to_raw());
+
+        self.consume_whitespace();
+        self.consume_token();
+        self.consume_whitespace();
+
+        if !self.at_token(Token::Newline) && !self.is_at_end() {
+            self.parse_separated_expressions(Token::Comma, SyntaxKind::ArgumentList);
+        }
+
+        self.builder.finish_node();
     }
 }
 
