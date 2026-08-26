8938e8e81cad4f983c0e71b77195feba4d8b5912
ScriptAndCompile <scriptandcompile@gmail.com>
2026-08-26 02:18:34 -0700

    moved tests for exit control_flow

---
+++ projects/vb6parse/tests/statements.rs
@@ -0,0 +1,7 @@
+// Statement parsing tests organized by statement category
+
+#[path = "statements/control_flow/exit.rs"]
+mod exit;
+
+#[path = "statements/control_flow/exit_resume.rs"]
+mod exit_resume;
