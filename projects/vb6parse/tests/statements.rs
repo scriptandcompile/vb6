94ed0467fc7f2e5b62bf6117f5d9ffddcf0c4491
ScriptAndCompile <scriptandcompile@gmail.com>
2026-08-26 03:41:09 -0700

    removed the duplication

--- projects/vb6parse/tests/statements.rs
+++ projects/vb6parse/tests/statements.rs
@@ -3,8 +3,5 @@
 #[path = "statements/control_flow/exit.rs"]
 mod exit;
 
-#[path = "statements/control_flow/exit_resume.rs"]
-mod exit_resume;
-
 #[path = "statements/control_flow/jump.rs"]
 mod jump;
